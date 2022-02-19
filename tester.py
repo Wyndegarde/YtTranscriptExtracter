5a6c68a4330af911e9aca582a35b488cd8434d66
Wyndegarde <76256133+Wyndegarde@users.noreply.github.com>
2022-02-19 15:19:00 +0000

    Delete tester.py

--- tester.py
+++
@@ -1,9 +0,0 @@
-from youtube_transcript_api import YouTubeTranscriptApi
-
-transcript = YouTubeTranscriptApi.get_transcript('MI0HoDiBABc')
-
-search_word = 'eren'
-
-for caption in transcript:
-    if search_word in caption['text']:
-        print(caption)
